ffd3fadde6c5e246b2f1bda52caef4ec1c17dc05
Sep7ic <91522111+Sep7ic@users.noreply.github.com>
2023-01-30 22:19:25 +1300

    a

--- public/script.js
+++ public/script.js
@@ -32,9 +32,8 @@ let navbar = `
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-content">
-    <a href="#">Site Map</a>
+    <a href="sitemap.html">Site Map</a>
     <a href="rss.xml">RSS Feed</a>
-    <a href="sillytest.html">test</a>
   </div>
 </div>
 `;
@@ -52,6 +51,7 @@ let header = `
 ███████║███████╗██║     
 ╚══════╝╚══════╝╚═╝     
 </pre>
+welcome, visitor #<span id="hitcount"><span>
 </div> 
 <img src="style/fleur.png" style="transform: scaleX(-1); float: right; padding: 10px;"/>
 &nbsp
@@ -71,7 +71,14 @@ let flags = `
 </div>
 `;
 
-let footer = '<span>this site was started</span> <abbr class="timeago" title="2023-01-23T15:33+13:00">Jan 23rd, 15:33</abbr>'
+let footer = `
+<div style="text-align: center; padding: 10px;">
+<span>this site was last updated</span> <abbr class="timeago" id="time" title="CANT FETCH">CANT FETCH</abbr>
+<br>
+<span>this site was started</span> <abbr class="timeago" id="started" title="CANT FETCH">CANT FETCH</abbr>
+</div>`
+
+
 
 document.head.innerHTML +=  `
 <title>sep</title>
@@ -91,6 +98,9 @@ if (document.getElementById("navbar")) {
 if (document.getElementById("flags")) {
   document.getElementById("flags").innerHTML = flags;
 }
+if (document.getElementById("footer")) {
+  document.getElementById("footer").innerHTML = footer;
+}
 
 
 
@@ -102,14 +112,44 @@ xhttp.onreadystatechange = function() {
         var num_arr = site_data.info.views.toString().split("");
         var num_str = "";
         for (i = 0; i < num_arr.length; i++) {
-        num_str += num_arr[i];
-        if ( (num_arr.length-1 - i) % 3 == 0 && (num_arr.length-1 - i) != 0 ) {num_str += ",";}
+            num_str += num_arr[i];
+            if ( (num_arr.length-1 - i) % 3 == 0 && (num_arr.length-1 - i) != 0 ) {num_str += ",";}
+            var date_str = site_data.info.last_updated;
+            var date_obj = new Date(site_data.info.last_updated);
+            var not_obj = new Date(site_data.info.created_at);
+            
+            const nth = function(o) {
+              if (o > 3 && o < 21) return 'th';
+              switch (o % 10) {
+                case 1:  return "st";
+                case 2:  return "nd";
+                case 3:  return "rd";
+                default: return "th";
+              }
+            }
+            const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            const datey = date_obj.getDate()
+
+            COOLTEXTHERE = monthNames[d.getMonth()] + " " + datey+nth(datey) + "," +not_obj.getHours() + ":" + ('0'+ date_obj.getMinutes()).slice(-2)
+
+            $("#time").text(COOLTEXTHERE)
+
+            nyehheh = date_obj.getMinutes()
+            cooldate = ('0'+nyehheh).slice(-2)
+            datesomething = date_obj.getFullYear() + "-" + (date_obj.getMonth()+1) + "-" +date_obj.getDate() + "T" + date_obj.getHours() + ":" + cooldate + "+13:00";
+            $("#time").attr('title', datesomething)
+
+            dateob = not_obj.getMinutes()
+            idk = ('0'+nyehheh).slice(-2)
+            acool = not_obj.getFullYear() + "-" + (not_obj.getMonth()+1) + "-" +not_obj.getDate() + "T" + not_obj.getHours() + ":" + idk + "+13:00";
+            $("#started").attr('title', acool)
+  
+          }
+        document.getElementById("hitcount").innerHTML = num_str;
     }
-document.getElementById("hitcount").innerHTML = num_str;
-    }
-  };
-  xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=sep", true);
-  xhttp.send();
+};
+xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=sep", true);
+xhttp.send();
   //----------------------------------------------//
 
   jQuery(document).ready(function() {
