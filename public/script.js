280ae04f49454dc92aedd5a7801304c95653961e
Sep7ic <91522111+Sep7ic@users.noreply.github.com>
2023-02-02 14:03:28 +1300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -83,6 +83,12 @@ document.head.innerHTML +=  `
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+<title>Website Name</title>
+<meta content="Embed Title" property="og:title" />
+<meta content="Site Description" property="og:description" />
+<meta content="https://embed.com/this-is-the-site-url" property="og:url" />
+<meta content="https://embed.com/embedimage.png" property="og:image" />
+<meta content="#43B581" data-react-helmet="true" name="theme-color" />
 `;
 
 
