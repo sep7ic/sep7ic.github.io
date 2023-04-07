ecebf7b2d4fdae47cc5c6e5b472ab44912cc2e08
Sep7ic <91522111+Sep7ic@users.noreply.github.com>
2023-04-07 15:17:39 +1200

    COOL!

--- public/code/lastfm.js
+++ public/code/lastfm.js
@@ -1,25 +1,46 @@
-$(document).ready(function() {
-    var user = $("#listen").attr("user")
-    var url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+user+"&api_key=afc213709a996ae561e307f596c9952b&format=json";
-    $.getJSON(url, function(data) {
+document.addEventListener('DOMContentLoaded', function() {
+  var user = document.getElementById("listen").getAttribute("user");
+  var url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + user + "&api_key=afc213709a996ae561e307f596c9952b&format=json";
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      
+      var data = JSON.parse(request.responseText);
       var artist = data.recenttracks.track[0].artist["#text"];
       var song = data.recenttracks.track[0]["name"];
       var artwork = data.recenttracks.track[0].image[1]["#text"];
       var link = data.recenttracks.track[0]["url"];
-  
-  
-      $("#artwork").attr("src", artwork);
-      $('#track').html("\
+
+      var artworkImg = document.getElementById("artwork");
+      artworkImg.setAttribute("src", artwork);
+
+      var trackInfo = document.getElementById("track");
+      trackInfo.innerHTML = "\
         <strong>"+song+"</strong><br />\
         "+artist+"\
-      ");
-      $("#artworklink").attr("href", link)
-  
-      if (typeof data.recenttracks.track[0]["@attr"] != "undefined"){
-              $("#listen").html('<span style="color: green;">listening</span> to:')
+      ";
+
+      var artworkLink = document.getElementById("artworklink");
+      artworkLink.setAttribute("href", link);
+
+      if (typeof data.recenttracks.track[0]["@attr"] !== "undefined"){
+        var listenInfo = document.getElementById("listen");
+        listenInfo.innerHTML = '<span style="color: green;">listening</span> to:';
       } else {
-        $("#listen").html("last <span>listened</span> to:")
-          console.log("false")
+        var listenInfo = document.getElementById("listen");
+        listenInfo.innerHTML = 'last <span>listened</span> to:';
+        console.log("false");
       }
-    });
-  });
+    } else {
+      console.error("Error fetching data from server.");
+    }
+  };
+
+  request.onerror = function() {
+    console.error("Connection error.");
+  };
+
+  request.send();
+});
