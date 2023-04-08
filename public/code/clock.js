19a7a17abbb188e5d7f80054719894f967dc91e2
Sep7ic <91522111+Sep7ic@users.noreply.github.com>
2023-04-08 13:19:32 +1200

    new

--- public/code/clock.js
+++ public/code/clock.js
@@ -1,35 +1,42 @@
-  function dateToText(date) {
-      var hours = date.getHours()
-      var minutes = date.getMinutes();
-       var seconds = date.getSeconds();
-      if (minutes < 10) minutes = '0'+minutes;
-      if  (seconds < 10) seconds = '0'+seconds;
-      if (hours < 10) hours = '0'+hours;
-      return hours + ":" + minutes + ":" + seconds;
+function dateToText(date, includeSeconds) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  if (minutes < 10) minutes = '0'+minutes;
+  if (seconds < 10) seconds = '0'+seconds;
+  if (hours < 10) hours = '0'+hours;
+  if (includeSeconds) {
+    return hours + ":" + minutes + ":" + seconds;
+  } else {
+    return hours + ":" + minutes;
   }
-  function updateClocks() {
-    for (var i = 0; i < window.arrClocks.length; i++) {
-      var clock = window.arrClocks[i];
-      var offset = window.arrOffsets[i];
-      clock.innerHTML = dateToText(new Date(new Date().getTime()+offset));
+}
+
+function updateClocks() {
+  for (var i = 0; i < window.arrClocks.length; i++) {
+    var clock = window.arrClocks[i];
+    var offset = window.arrOffsets[i];
+    var includeSeconds = clock.classList.contains('clockseconds');
+    clock.innerHTML = dateToText(new Date(new Date().getTime()+offset), includeSeconds);
+  }
+}
+
+function startClocks() {
+  var clockElements = document.querySelectorAll('.clock, .clockseconds');
+  window.arrClocks = [];
+  window.arrOffsets = [];
+  for(var i = 0; i < clockElements.length; i++) {
+    var el = clockElements[i];
+    var timezone = parseInt(el.getAttribute('timezone'));
+    if (!isNaN(timezone)) {
+      var tzDifference = timezone * 60 + (new Date()).getTimezoneOffset();
+      var offset = tzDifference * 60 * 1000;
+      window.arrClocks.push(el);
+      window.arrOffsets.push(offset);
     }
   }
-  function startClocks() {
-    clockElements = document.getElementsByClassName('clock');
-    window.arrClocks = []
-    window.arrOffsets = [];
-    var j = 0;
-    for(var i = 0; i < clockElements.length; i++) {
-      el = clockElements[i];
-      timezone = parseInt(el.getAttribute('timezone'));
-      if (!isNaN(timezone)) {
-        var tzDifference = timezone * 60 + (new Date()).getTimezoneOffset();
-        var offset = tzDifference * 60 * 1000;
-        window.arrClocks.push(el);
-        window.arrOffsets.push(offset);
-      }
-    }
-    updateClocks();
-    clockID = setInterval(updateClocks, 1000);
-  }
-  setTimeout(startClocks, 100);
+  updateClocks();
+  clockID = setInterval(updateClocks, 1000);
+}
+
+setTimeout(startClocks, 100);
