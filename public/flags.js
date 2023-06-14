71732e43ae7d5376fbad8288a9fe1fd2142a9ff2
Sep7ic <91522111+Sep7ic@users.noreply.github.com>
2023-06-14 13:29:40 +1200

    sad face please work

--- public/flags.js
+++ public/flags.js
@@ -25,113 +25,59 @@ const firebaseConfig = {
       }
     }
     
-    let verifiedusers = ["cyaT5DGKLAa1UwVD3DEz37ddqgg2", "Hzy5uRYmgROxsGUoQ8pgTTZxNwO2"];
-
     // Send a new message to the chat
     function sendMessage() {
       setDisplayNameFromMessage();
       const message = document.getElementById("message").value;
-      const uid = auth.currentUser.uid;
-      const messagesRef = firebase.database().ref("chat");
-    
-      // Check if the user is banned
-      firebase.database().ref("banned").child(uid).once("value")
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            console.log("User is banned");
-            return;
-          }
-    
-          if (message !== "") {
-            if (message.startsWith("/nick ")) {
-              const displayName = message.substr(6).trim();
-              const user = auth.currentUser;
-              const uid = user.uid;
-              const messagesRef = firebase.database().ref("chat");
-              messagesRef.once("value", (snapshot) => {
-                snapshot.forEach((childSnapshot) => {
-                  const messageId = childSnapshot.key;
-                  const messageData = childSnapshot.val();
-                  if (messageData.sender === uid) {
-                    firebase.database().ref("chat/" + messageId + "/senderName").set(displayName);
-                  }
-                });
-              });
-              user.updateProfile({
-                displayName: displayName
-              }).then(() => {
-                console.log("Display name updated successfully");
-                document.getElementById("message").value = "";
-                location.reload(); // Reload the page
-              }).catch((error) => {
-                console.log(error.message);
-              });
-            } else if (message.startsWith("/auth")) {
-              auth.signInAnonymously()
-                .then(() => {
-                  console.log("User signed in anonymously");
-                  document.getElementById("message").value = "";
-                })
-                .catch((error) => {
-                  console.log(error.message);
-                });
-            } else if (message.startsWith("/ban ")) {
-              // handle /ban command
-              const uidToBan = message.substr(5).trim();
-              const user = auth.currentUser;
-
-              if (verifiedusers.includes(auth.currentUser.uid)) {
-                const banListRef = firebase.database().ref("banList");
-                banListRef.push().set({
-                  uid: uidToBan
-                }).then(() => {
-                  console.log(`User ${uidToBan} has been banned`);
-                  document.getElementById("message").value = "";
-                }).catch((error) => {
-                  console.log(error.message);
-                });
-              } else {
-                console.log("You are not authorized to use this command");
-                document.getElementById("message").value = "";
+      if (message !== "") {
+        if (message.startsWith("/nick ")) {
+          const displayName = message.substr(6).trim();
+          const user = auth.currentUser;
+          const uid = user.uid;
+          const messagesRef = firebase.database().ref("chat");
+          messagesRef.once("value", (snapshot) => {
+            snapshot.forEach((childSnapshot) => {
+              const messageId = childSnapshot.key;
+              const messageData = childSnapshot.val();
+              if (messageData.sender === uid) {
+                firebase.database().ref("chat/" + messageId + "/senderName").set(displayName);
               }
-            } else if (verifiedusers.includes(auth.currentUser.uid) && message.startsWith("/lookup ")) {
-              const senderName = message.substr(8).trim();
-              chatRef.once("value", (snapshot) => { 
-                let uid = null;
-                snapshot.forEach((childSnapshot) => {
-                  const messageData = childSnapshot.val();
-                  if (messageData.senderName === senderName) {
-                    uid = messageData.sender;
-                  }
-                });
-                if (uid !== null) {
-                  console.log(`UID for ${senderName} is ${uid}`);
-                } else {
-                  console.log(`No user found with name ${senderName}`);
-                }
-                document.getElementById("message").value = "";
-              });
-              } else {
-              const name = auth.currentUser.displayName || "Anonymous";
-              const now = new Date();
-              const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland" });
-              const time = now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" });
-              chatRef.push().set({
-                sender: uid,
-                senderName: name,
-                text: message,
-                date: date,
-                time: time
-              });
+            });
+          });
+          user.updateProfile({
+            displayName: displayName
+          }).then(() => {
+            console.log("Display name updated successfully");
+            document.getElementById("message").value = "";
+            location.reload(); // Reload the page
+          }).catch((error) => {
+            console.log(error.message);
+          });
+        } else if (message.startsWith("/auth")) {
+          auth.signInAnonymously()
+            .then(() => {
+              console.log("User signed in anonymously");
               document.getElementById("message").value = "";
-            }
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
+            })
+            .catch((error) => {
+              console.log(error.message);
+            });
+        } else {
+          const name = auth.currentUser.displayName || "Anonymous";
+          const now = new Date();
+          const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland" });
+          const time = now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" });
+          chatRef.push().set({
+            sender: auth.currentUser.uid,
+            senderName: name,
+            text: message,
+            date: date,
+            time: time
+          });
+          document.getElementById("message").value = "";
+        }
+      }
     }
-    
     
 
 function setDisplayNameFromMessage() {
@@ -173,7 +119,7 @@ const senderElement = document.createElement("span");
 senderElement.innerText = senderName + ": ";
 
 // Set the class of the sender element based on the senderUid
-if (verifiedusers.includes(senderUid)) {
+if (senderUid === "wkvuyfySZLPNWGyUl6WvwZkKBNu2") {
   senderElement.classList.add("red-sender"); // add a CSS class to change color to red
 } else {
   senderElement.classList.add("grey-sender"); // add a CSS class to change color to grey
