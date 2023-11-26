42bbdf6e1e18d7a9150ce68043454f6b82cb971d
septic <91522111+sep7ic@users.noreply.github.com>
2023-11-27 11:41:48 +1300

    psu

--- public/js/flags.js
+++ public/js/flags.js
@@ -45,10 +45,14 @@ function sendMessage() {
         });
     } else {
       if (!auth.currentUser){
-  const authinit = document.createElement("span");
-  authinit.innerHTML = '<span style="color: DimGrey;">you must first run /auth</span><br>';
-  chat.insertBefore(authinit, chat.firstChild);
-}
+        const authinit = document.createElement("span");
+        authinit.innerHTML = '<span style="color: DimGrey;">you must first run /auth</span><br>';
+        chat.insertBefore(authinit, chat.firstChild);
+    } else {
+      const msgsent = document.createElement("span");
+      msgsent.innerHTML = '<span style="color: DimGrey;">message sent!</span><br>';
+        chat.insertBefore(msgsent, chat.firstChild);
+    }
       const name = auth.currentUser.displayName || "Anonymous";
       const now = new Date();
       const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland" });
