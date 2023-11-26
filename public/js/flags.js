f69640a9d4ccee5f9fbc5e24bbe46783bd314b1e
septic <91522111+sep7ic@users.noreply.github.com>
2023-11-26 00:51:52 +0000

    push

--- public/js/flags.js
+++ public/js/flags.js
@@ -6,84 +6,76 @@ const firebaseConfig = {
   storageBucket: "septic-3172a.appspot.com",
   messagingSenderId: "279553956146",
   appId: "1:279553956146:web:4a345eeb09388b7b13df2b"
-};
-    firebase.initializeApp(firebaseConfig);
-    const database = firebase.database();
-    const chatRef = database.ref("chat");
-    const auth = firebase.auth();
+};firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const chatRef = database.ref("chat");
+const auth = firebase.auth();
 
-    // Listen for new messages in the chat
-    chatRef.on("child_added", function(snapshot) {
-      const message = snapshot.val();
-      displayMessage(message.senderName, message.text, message.date, message.time, message.sender); // pass senderUid as the last argument
-    });
-    
+chatRef.on("child_added", function(snapshot) {
+  const message = snapshot.val();
+  displayMessage(message.senderName, message.text, message.date, message.time, message.sender, message.color);
+});
 
-    function handleKeyDown(event) {
-      if (event.key === "Enter" || event.keyCode === 13 || event.keyCode === 9) {
-        sendMessage();
-      }
-    }
-    
-    // Send a new message to the chat
-    function sendMessage() {
-      setDisplayNameFromMessage();
-      const message = document.getElementById("message").value;
-      if (message !== "") {
-        if (message.startsWith("/nick ")) {
-          const displayName = message.substr(6).trim();
-          const user = auth.currentUser;
-          const uid = user.uid;
-          const messagesRef = firebase.database().ref("chat");
-          messagesRef.once("value", (snapshot) => {
-            snapshot.forEach((childSnapshot) => {
-              const messageId = childSnapshot.key;
-              const messageData = childSnapshot.val();
-              if (messageData.sender === uid) {
-                firebase.database().ref("chat/" + messageId + "/senderName").set(displayName);
-              }
-            });
-          });
-          user.updateProfile({
-            displayName: displayName
-          }).then(() => {
-            console.log("Display name updated successfully");
-            document.getElementById("message").value = "";
-            location.reload(); // Reload the page
-          }).catch((error) => {
-            console.log(error.message);
-          });
-        } else if (message.startsWith("/auth")) {
-          auth.signInAnonymously()
-            .then(() => {
-              console.log("User signed in anonymously");
-              document.getElementById("message").value = "";
-            })
-            .catch((error) => {
-              console.log(error.message);
-            });
-        } else {
-          const name = auth.currentUser.displayName || "Anonymous";
-          const now = new Date();
-          const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland" });
-          const time = now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland", hour12: false });
-          chatRef.push().set({
-            sender: auth.currentUser.uid,
-            senderName: name,
-            text: message,
-            date: date,
-            time: time
-          });
+function handleKeyDown(event) {
+  if (event.key === "Enter" || event.keyCode === 13 || event.keyCode === 9) {
+    sendMessage();
+  }
+}
+
+function sendMessage() {
+  setDisplayNameFromMessage();
+  const message = document.getElementById("message").value;
+  const blockedUserId = "xQwICs1DTRZGTwfFR05LOfPaicy1";
+  if (auth.currentUser.uid === blockedUserId) {
+    console.log("You are blocked and cannot send messages.");
+    return;
+  }
+  if (message !== "") {
+    if (message.startsWith("/nick ")) {
+      const displayName = message.substr(6).trim();
+      updateDisplayName(displayName);
+    } else if (message.startsWith("/colour ")) {
+      const color = message.substr(8).trim();
+      updateColor(color);
+    } else if (message.startsWith("/auth")) {
+      auth.signInAnonymously()
+        .then(() => {
+          console.log("User signed in anonymously");
           document.getElementById("message").value = "";
-        }
-      }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } else {
+      const name = auth.currentUser.displayName || "Anonymous";
+      const now = new Date();
+      const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland" });
+      const time = now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland", hour12: false });
+      const timestamp = Date.now(); 
+      const color = getUserColor(auth.currentUser.uid);
+      chatRef.push().set({
+        sender: auth.currentUser.uid,
+        senderName: name,
+        text: message,
+        date: date,
+        time: time,
+        color: color,
+        timestamp: timestamp,
+      });
+      document.getElementById("message").value = "";
     }
-    
+  }
+}
 
 function setDisplayNameFromMessage() {
-const message = document.getElementById("message").value;
-if (message.startsWith("/nick ")) {
-  const displayName = message.substr(6).trim();
+  const message = document.getElementById("message").value;
+  if (message.startsWith("/nick ")) {
+    const displayName = message.substr(6).trim();
+    updateDisplayName(displayName);
+  }
+}
+
+function updateDisplayName(displayName) {
   const user = auth.currentUser;
   const uid = user.uid;
   const messagesRef = firebase.database().ref("chat");
@@ -101,36 +93,62 @@ if (message.startsWith("/nick ")) {
   }).then(() => {
     console.log("Display name updated successfully");
     document.getElementById("message").value = "";
-    location.reload(); // Reload the page
+    location.reload();
   }).catch((error) => {
     console.log(error.message);
   });
 }
+
+function updateColor(color) {
+  const user = auth.currentUser;
+  const uid = user.uid;
+  const messagesRef = firebase.database().ref("chat");
+  messagesRef.once("value", (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const messageId = childSnapshot.key;
+      const messageData = childSnapshot.val();
+      if (messageData.sender === uid) {
+        firebase.database().ref("chat/" + messageId + "/color").set(color);
+      }
+    });
+  });
+  document.getElementById("message").value = "";
 }
 
+function getUserColor(uid) {
+  const defaultColor = "black"; 
+  const messagesRef = firebase.database().ref("chat");
+  let userColor = defaultColor;
 
-let verifiedusers = ["bq7qsGSJ1eQJTbkTi7VpkNscW4d2", "cyaT5DGKLAa1UwVD3DEz37ddqgg2"];
+  messagesRef.once("value", (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const messageData = childSnapshot.val();
+      if (messageData.sender === uid && messageData.color) {
+        userColor = messageData.color;
+      }
+    });
+  });
 
-function displayMessage(senderName, text, date, time, senderUid) {
+  return userColor;
+}
+
+let verifiedusers = ["vHnehfBgx1UiBkJu40Qu2CBAxsP2", "cyaT5DGKLAa1UwVD3DEz37ddqgg2"];
+
+function displayMessage(senderName, text, date, time, senderUid, color) {
   const chat = document.getElementById("chat");
   const messageElement = document.createElement("div");
-  
-  // Create a span element for the sender name
-  const senderElement = document.createElement("span");
-  senderElement.innerHTML = '<span style="color: DimGray; font-size: smaller;">' + date + ", " + time + " | " + "</span>" + senderName + ": ";
 
-  // Set the class of the sender element based on the senderUid
+  const senderElement = document.createElement("span");
+  senderElement.innerHTML = '<span style="color: DimGrey; font-size: smaller;">' + date + ", " + time + " | " + "</span>" + '<span style="color:' + color + ';">' + senderName + ": </span>";
+
   if (verifiedusers.includes(senderUid)) {
-    senderElement.classList.add("red-sender"); // add a CSS class to change color to red
+    senderElement.classList.add("red-sender");
   } else {
-    senderElement.classList.add("grey-sender"); // add a CSS class to change color to grey
+    senderElement.classList.add("grey-sender");
   }
-  
-  // Add the sender and text to the message element
+
   messageElement.appendChild(senderElement);
   messageElement.appendChild(document.createTextNode(text));
-  
+
   chat.insertBefore(messageElement, chat.firstChild);
 }
-
-    
