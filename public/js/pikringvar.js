c250427381b14af7f6c210f7dd80e9f81a6a895f
septic <91522111+sep7ic@users.noreply.github.com>
2023-11-28 22:10:17 +1300

    aa

---
+++ public/js/pikringvar.js
@@ -0,0 +1,22 @@
+var sites = [
+    'https://nickolox.neocities.org',
+    'https://angelscake.neocities.org',
+    'https://candypop-garden.neocities.org',
+    'https://doqmeat.neocities.org',
+    'https://endski.neocities.org',
+    'https://oudkee.neocities.org',
+    'https://superkirbylover.neocities.org',
+    'https://thepersonever.neocities.org',
+    'https://jeith.neocities.org',
+    'https://madeinv.neocities.org',
+    'https://bruisedgh0st.neocities.org',
+    'https://joxi.neocities.org',
+    'https://sep.neocities.org',
+    'https://septic.lol',
+    'http://127.0.0.1:5500/'
+    ];
+    var ringName = 'Pik Ring';
+    var ringID = 'PikRing';
+    var indexPage = 'https://nickolox.neocities.org/pikring/';
+    var useIndex = true;
+    var useRandom = true;
